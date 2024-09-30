3270846ff116138bde109734b9122f9cc78df9f4
ravi-sam-wedding <ravi.sam.wedding@gmail.com>
2024-09-29 17:13:43 -0700

    updating google api for rsvp.

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxb0hn3QDVrZAmInDauglWN7husTUTReffVbb9f2FRija7KCMBaypbJzfQRNQCJi563/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyi_NC30yY21CXWj0vbddY3ZezlwwOFCszRjfj1LpdJ7MV-EmUfn-5kzBpPNxjA8PZ7WQ/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
