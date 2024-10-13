5a2cec0b82e988a02596edb4f01fb5be0f628dde
ravi-sam-wedding <ravi.sam.wedding@gmail.com>
2024-10-13 14:44:14 -0700

    setting up pipeline for integration tests.

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "tests/integrationTests/**/*.test.js",
+    supportFile: false
+  }
+})
---
+++ tests/integrationTests/rsvpForm.test.js
@@ -0,0 +1,5 @@
+describe('Always Passes', () => {
+    it('should always pass', () => {
+      expect(true).to.equal(true);
+    });
+  });
